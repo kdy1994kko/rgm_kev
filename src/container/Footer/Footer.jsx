0d23639c3d2d59363cc083bde53494490018f0b0
Kevvan D. Young <101077732+kdy1994kko@users.noreply.github.com>
2023-09-28 10:12:51 -0600

    Add files via upload

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FiFacebook, FiLinkedin, FiInstagram } from 'react-icons/fi';
+import { FiFacebook, FiLinkedin, FiInstagram, FiGithub} from 'react-icons/fi';
 
 import { FooterOverlay, Newsletter } from '../../components';
 import { images } from '../../constants';
@@ -9,31 +9,34 @@ const Footer = () => (
   <div className="app__footer section__padding" id="login">
     <FooterOverlay />
     <Newsletter />
-
     <div className="app__footer-links">
+      <div className="app__footer-links_logo">
+        <p className="p__opensans"><a href="https://www.biblegateway.com/passage/?search=1+john+5%3A14&version=NIV" target="_blank" rel="noreferrer">&quot;This is the confidence we have in approaching God: that if we ask anything according to his will, he hears us.&quot;</a></p>
+        <p className="p__opensans"><a href="https://www.biblegateway.com/passage/?search=1+john+5%3A14&version=NIV" target="_blank" rel="noreferrer">~ 1 john 5:14</a></p>
+        <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} />
+      </div>
+        <div className="app__footer-links_icons">
+          <a href="https://www.facebook.com/kevvankko.young/" target="_blank" rel="noreferrer">
+            <FiFacebook />
+          </a>
+          <a href="https://www.linkedin.com/in/kevvan-d-young/" target="_blank" rel="noreferrer">
+            <FiLinkedin />
+          </a>
+          <a href="https://instagram.com/blvck.kev44?igshid=OGQ5ZDc2ODk2ZA==" target="_blank" rel="noreferrer">
+            <FiInstagram />
+          </a>
+          <a href="https://github.com/kdy1994kko" target="_blank" rel="noreferrer">
+            <FiGithub />
+          </a>
+        </div>
       <div className="app__footer-links_contact">
         <h1 className="app__footer-headtext">Hire Me</h1>
         <p className="p__opensans">16092 Crossed talons rd, Woodbridge, VA, 22193 USA</p>
         <p className="p__opensans">+1 281-896-2303</p>
-      </div>
-        <div className="app__footer-links_icons">
-          <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
-            <FiFacebook />
-          </a>
-          <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
-            <FiLinkedin />
-          </a>
-          <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
-            <FiInstagram />
-          </a>
-        </div>
-      <div className="app__footer-links_logo">
-        <img src={images.gericht} alt="footer_logo" />
-        <p className="p__opensans">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
-        <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} />
       </div>
     </div>
   </div>
 );
 
 export default Footer;
+
